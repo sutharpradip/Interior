96f5d80185dddde8ae72fc5c67720424f82d62a4
sutharpradip <pradeepsutharr7@gmail.com>
2025-02-17 21:19:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   base: './', // Ensures relative paths for assets
+//   build: {
+//     outDir: 'dist',
+//   },
+// });
 export default defineConfig({
-  base: './', // Ensures relative paths for assets
-  build: {
-    outDir: 'dist',
-  },
+  base: '/', // Set this correctly
+  plugins: [react()],
 });
