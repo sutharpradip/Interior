7c502e3ea6490f334c54ff6531d0adaff425e2c7
pradeepsutharr <psuthar318@gmail.com>
2025-02-17 22:37:39 +0530

    build

--- src/Components/WhyChooseUs.jsx
+++ src/Components/WhyChooseUs.jsx
@@ -17,35 +17,9 @@ function WhyChooseUs() {
             <div className="flex flex-wrap gap-y-8 my-10">
               <div className="w-full md:w-1/2 p-2">
                 <div className="icon mb-3 relative before:z-[-1] before:block before:top-2 before:left-5 z-10 before:w-9 before:rounded-full before:h-9 before:absolute before:bg-gray-300">
-                  <img className="" src="src/assets/images/truck.svg" alt="" />
-                </div>
-                <h3 className="my-1 font-medium text-gray-800">
-                  Fast & Free Shipping
-                </h3>
-                <p className="text-gray-500 text-sm font-normal">
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-
-              <div className="w-full md:w-1/2 p-2">
-                <div className="icon mb-3 relative before:z-[-1] before:block before:top-2 before:left-5 z-10 before:w-9 before:rounded-full before:h-9 before:absolute before:bg-gray-300">
-                  <img className="" src="src/assets/images/bag.svg" alt="" />
-                </div>
-                <h3 className="my-1 font-medium text-gray-800">
-                  Fast & Free Shipping
-                </h3>
-                <p className="text-gray-500 text-sm font-normal">
-                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
-                  aliquet velit. Aliquam vulputate.
-                </p>
-              </div>
-
-              <div className="w-full md:w-1/2 p-2">
-                <div className="icon mb-3 relative before:z-[-1] before:block before:top-2 before:left-5 z-10 before:w-9 before:rounded-full before:h-9 before:absolute before:bg-gray-300">
                   <img
                     className=""
-                    src="src/assets/images/support.svg"
+                    src="public/assets/images/truck.svg"
                     alt=""
                   />
                 </div>
@@ -60,7 +34,41 @@ function WhyChooseUs() {
 
               <div className="w-full md:w-1/2 p-2">
                 <div className="icon mb-3 relative before:z-[-1] before:block before:top-2 before:left-5 z-10 before:w-9 before:rounded-full before:h-9 before:absolute before:bg-gray-300">
-                  <img className="" src="src/assets/images/return.svg" alt="" />
+                  <img className="" src="public/assets/images/bag.svg" alt="" />
+                </div>
+                <h3 className="my-1 font-medium text-gray-800">
+                  Fast & Free Shipping
+                </h3>
+                <p className="text-gray-500 text-sm font-normal">
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+
+              <div className="w-full md:w-1/2 p-2">
+                <div className="icon mb-3 relative before:z-[-1] before:block before:top-2 before:left-5 z-10 before:w-9 before:rounded-full before:h-9 before:absolute before:bg-gray-300">
+                  <img
+                    className=""
+                    src="public/assets/images/support.svg"
+                    alt=""
+                  />
+                </div>
+                <h3 className="my-1 font-medium text-gray-800">
+                  Fast & Free Shipping
+                </h3>
+                <p className="text-gray-500 text-sm font-normal">
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+
+              <div className="w-full md:w-1/2 p-2">
+                <div className="icon mb-3 relative before:z-[-1] before:block before:top-2 before:left-5 z-10 before:w-9 before:rounded-full before:h-9 before:absolute before:bg-gray-300">
+                  <img
+                    className=""
+                    src="public/assets/images/return.svg"
+                    alt=""
+                  />
                 </div>
                 <h3 className="my-1 font-medium text-gray-800">
                   Fast & Free Shipping
@@ -77,7 +85,7 @@ function WhyChooseUs() {
             <div className="img-div why-choose-before relative z-10">
               <img
                 className="rounded-3xl"
-                src="src/assets/images/why-choose-us-img.jpg"
+                src="public/assets/images/why-choose-us-img.jpg"
                 alt=""
               />
             </div>
