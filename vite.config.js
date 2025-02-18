80cf8d8d66c526395299b7e53bb524e3845e200e
sutharpradip <pradeepsutharr7@gmail.com>
2025-02-18 09:38:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/myapp/', // Ensure assets load correctly
+  base: './', // Ensure assets load correctly
   plugins: [react()],
 });
 
